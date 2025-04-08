36a21f9a5af7e135c69918f0e43973f1b42a55d6
Anton Abdrakhmanov <ode15s@yandex.ru>
2025-04-08 09:09:42 +0300

    Rename confident interval module file

--- Models/__init__.py
+++ Models/__init__.py
@@ -6,5 +6,8 @@ from . import TripleExpoSmooth
 from . import HoltWinters
 from . import HoltWinters2
 from . import HoltWinters3
-from . import SARIMAX
+from . import SARIMAX, SARIMAX_GS, SARIMAX_GS_2, SARIMAX_GS_smart, SARIMAX_GS_JL, SARIMAX_GridSearch_Joblib
+from . import LSTM_1, LSTM_2
+from . import XGBoostRegressor, XGBRegressor3, XGBRegressor2
+from . import auto_sarima_pmdarima
 
